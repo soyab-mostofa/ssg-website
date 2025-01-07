7cca132784cd66ee3bda8322c8f82ed695b95851
Soyab Mostofa <soyabmostofa@gmail.com>
2025-01-08 00:53:38 +0600

    add maps

---
+++ src/app/(frontend)/contact/MapLocation.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { MapContainer, TileLayer, Marker, Tooltip } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import { Icon } from 'leaflet'
+
+const skater = new Icon({
+  iconUrl: '/logo.png',
+  iconSize: [50, 50],
+})
+const DhakaMap = () => {
+  // Dhaka coordinates
+  return (
+    <div className="h-96 w-full overflow-hidden rounded-lg shadow-lg">
+      <MapContainer
+        className="h-full"
+        center={[23.88733117415661, 90.38951472552638]}
+        zoom={18}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          // className="h-full"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker
+          icon={skater}
+          title="Shin Shin Group"
+          position={[23.88733117415661, 90.38951472552638]}
+        >
+          <Tooltip className="bg-primary-blue-300/35" permanent>
+            Shin Shin Group
+          </Tooltip>
+        </Marker>
+      </MapContainer>
+    </div>
+  )
+}
+
+export default DhakaMap
--- src/app/(frontend)/contact/page.tsx
+++ src/app/(frontend)/contact/page.tsx
@@ -1,6 +1,9 @@
+'use client'
 import AboutHeader from '@/app/_components/pages/about/AboutHeader'
+
 import React from 'react'
 import ContactInfo from './ContactInfo'
+import DhakaMap from './MapLocation'
 
 const page = () => {
   return (
@@ -10,6 +13,7 @@ const page = () => {
         sub="Shin Shin Group is one of the largest conglomerates in Bangladesh, exporting apparel worldwide. The group comprises five factories."
       />
       <ContactInfo />
+      <DhakaMap />
     </div>
   )
 }
